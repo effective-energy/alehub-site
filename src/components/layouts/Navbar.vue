24b713f561e1b08fe66af067a0a1fe94faa8ef15
sergeevagalina <sergeevagalina.al@gmail.com>
2018-04-13 19:16:47 +0300

    Fix style for Navbar.vue

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,46 +1,44 @@
 <template>
 	<b-navbar toggleable="md" variant="info" class="navbar">
-		<b-container>
-			<b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-			<b-navbar-brand href="#" class="logo">
-		      <img src="../../assets/img/logo.png" alt="BV">
-		  	</b-navbar-brand>
+		<b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+		<b-navbar-brand href="#" class="logo">
+		<img src="../../assets/img/logo.png" alt="BV">
+		</b-navbar-brand>
 
-			<b-collapse is-nav id="nav_collapse">
+		<b-collapse is-nav id="nav_collapse">
 
-				<b-navbar-nav>
-					<b-nav-item href="#" class="active">Home</b-nav-item>
-					<b-nav-item href="#">Description</b-nav-item>
-					<b-nav-item href="#">Advantages</b-nav-item>
-					<b-nav-item href="#">Features</b-nav-item>
-					<b-nav-item href="#">Bounty</b-nav-item>
-					<b-nav-item href="#">Team</b-nav-item>
-					<b-nav-item href="#">News</b-nav-item>
-				</b-navbar-nav>
+			<b-navbar-nav>
+				<b-nav-item href="#" class="active">Home</b-nav-item>
+				<b-nav-item href="#">Description</b-nav-item>
+				<b-nav-item href="#">Advantages</b-nav-item>
+				<b-nav-item href="#">Features</b-nav-item>
+				<b-nav-item href="#">Bounty</b-nav-item>
+				<b-nav-item href="#">Team</b-nav-item>
+				<b-nav-item href="#">News</b-nav-item>
+			</b-navbar-nav>
 
-				<b-navbar-nav class="ml-auto">
+			<b-navbar-nav class="ml-auto">
 
-					<b-button size="sm" class="login-btn" variant="warning">Log in</b-button>
+				<b-button size="sm" class="login-btn" variant="warning">Log in</b-button>
 
-					<b-nav-item-dropdown
-						:text="selectedLanguage"
-						right
-						class="language-dropdown"
+				<b-nav-item-dropdown
+					:text="selectedLanguage"
+					right
+					class="language-dropdown"
+				>
+					<b-dropdown-item
+						:active="selectedLanguage === lang"
+						v-for="(lang, langIndex) in languageList"
+						:key="langIndex"
+						@click="selectLang(langIndex)"
 					>
-						<b-dropdown-item
-							:active="selectedLanguage === lang"
-							v-for="(lang, langIndex) in languageList"
-							:key="langIndex"
-							@click="selectLang(langIndex)"
-						>
-							{{ lang }}
-						</b-dropdown-item>
-					</b-nav-item-dropdown>
+						{{ lang }}
+					</b-dropdown-item>
+				</b-nav-item-dropdown>
 
-				</b-navbar-nav>
+			</b-navbar-nav>
 
-			</b-collapse>
-		</b-container>
+		</b-collapse>
 	</b-navbar>
 </template>
 
@@ -111,7 +109,10 @@
 			.navbar-nav
 				.nav-item:first-child
 					.nav-link
-						margin-right 0
+						margin-left 0
+
+			.navbar-brand
+				margin-right 0
 
 	@media(max-width: 767px)
 		.navbar
