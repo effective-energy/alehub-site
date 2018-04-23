a1d24ffa294df844154f3726b28d95a4d3f55afa
Denis <denisqwe41@gmail.com>
2018-04-23 20:24:50 +0300

    Update template

--- src/components/Screen1.vue
+++ src/components/Screen1.vue
@@ -41,6 +41,47 @@
                         <span>Bonus 25% up to 7 April 2018</span>
                         <span>Now not available</span>
                     </div>
+
+                    <div class="crypto">
+                        <div class="title">Accepted for collection:</div>
+                        <div class="collection">
+                            <div class="item">
+                                <img src="../../static/images/btc.svg" alt="">
+                                <div class="description">
+                                    <span class="count">2,389 ALE</span>
+                                    <span class="name">BTC</span>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="../../static/images/eth.svg" alt="">
+                                <div class="description">
+                                    <span class="count">1,337 ALE</span>
+                                    <span class="name">ETH</span>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="../../static/images/bch.svg" alt="">
+                                <div class="description">
+                                    <span class="count">2,228 ALE</span>
+                                    <span class="name">BCH</span>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="../../static/images/ltc.svg" alt="">
+                                <div class="description">
+                                    <span class="count">1,488 ALE</span>
+                                    <span class="name">LTC</span>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="../../static/images/dash.svg" alt="">
+                                <div class="description">
+                                    <span class="count">2,228 ALE</span>
+                                    <span class="name">DASH</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
