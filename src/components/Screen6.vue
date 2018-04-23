785c8f4343825554b1f88d38a6d0a43cecef66e4
Alexandr Voroncov <defauthack@gmail.com>
2018-04-23 22:52:48 +0300

    Update template

--- src/components/Screen6.vue
+++ src/components/Screen6.vue
@@ -1,13 +1,144 @@
 <template>
-    
+    <div class="section economy-section">
+    	<div class="container">
+	    	<h1 class="section-title is-center">
+	    		Economy of ICO
+	    	</h1>
+	    	<p class="section-subtitle is-center">
+	    		<span class="bold">ALE</span> — security token. To ensure liquidity and fast placement on exchanges, the <span class="bold">ALE</span> protocol is mirrored to the Ethereum according to the ERC20 standard
+	    	</p>
+
+	    	<div class="row figures-panel">
+	    		<div class="col-4 is-center is-white">
+	    			<h1 class="figures-title">Emission</h1>
+	    			<h3 class="figures-subtitle">100,500,000,000 ALE</h3>
+	    		</div>
+	    		<div class="col-4 is-center is-white">
+	    			<h1 class="figures-title">Hardcap</h1>
+	    			<h3 class="figures-subtitle">77,000 ETH</h3>
+	    		</div>
+	    		<div class="col-4 is-center is-white">
+	    			<h1 class="figures-title">Softcap</h1>
+	    			<h3 class="figures-subtitle">4,500 ETH</h3>
+	    		</div>
+	    	</div>
+
+	    	<div class="row distribution">
+	    		<div class="col-6">
+	    			<div class="steep-statis">
+	    				
+	    			</div>
+	    		</div>
+	    		<div class="col-6">
+	    			<h1 class="section-title is-white is-right">Distribution of tokens</h1>
+	    			<div class="divider"></div>
+	    			<p class="small">
+	    				The ALE product is primarily a service for consumers to find counterparties for projects in the IT field and to manage these projects at the management and financial level. On the one hand, they are programmers or their associations, and on the other hand, they are IT Customers.
+	    			</p>
+	    		</div>
+	    	</div>
+	    </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Screen6'
+    name: 'Economy'
 }
 </script>
 
-<style lang="stylus">
-    
+<style lang="stylus" scoped>
+
+    .section
+    	padding 71px 0
+    	padding-bottom 130px
+
+    .economy-section
+    	background-color #343a49
+
+    .is-center
+    	text-align center
+
+    .divider
+    	width 120px
+    	height 2px
+    	background-color #ffffff
+    	margin-bottom 30px
+
+    .is-white
+    	color #ffffff
+
+    .is-right
+    	text-align left
+
+    .section-title
+    	font-family MuseoSansCyrl300
+    	font-size 3em
+    	font-weight 300
+    	font-style normal
+    	font-stretch normal
+    	line-height normal
+    	letter-spacing normal
+    	color #ffffff
+    	margin 24px 0
+
+    .section-subtitle
+    	font-family MuseoSansCyrl300
+    	font-size 1.4em
+    	font-weight 300
+    	font-style normal
+    	font-stretch normal
+    	line-height 1.5
+    	letter-spacing normal
+    	text-align center
+    	color #ffffff
+    	padding 12px 0 24px 0
+
+    	.bold
+    		font-family MuseoSansCyrl500
+    		font-weight bold
+
+    .figures-panel
+    	padding 24px 0 71px 0
+
+    	.figures-title
+    		font-family MuseoSansCyrl500
+    		font-size 24px
+    		font-weight 500
+    		font-style normal
+    		font-stretch normal
+    		line-height 1.5
+    		letter-spacing normal
+    		text-align center
+    		color #ffffff
+    		margin 0 0 24px 0
+
+    	.figures-subtitle
+    		font-family MuseoSansCyrl300
+    		font-size 32px
+    		font-weight 300
+    		font-style normal
+    		font-stretch normal
+    		line-height 0.82
+    		letter-spacing normal
+    		text-align center
+    		color #ffffff
+
+    .distribution
+    	.small
+    		font-family MuseoSansCyrl300
+    		font-size 14px
+    		font-weight 300
+    		font-style normal
+    		font-stretch normal
+    		line-height 1.67
+    		letter-spacing normal
+    		text-align left
+    		color #ffffff
+
+    	.steep-statis
+    		width 280px
+    		height 280px
+    		background-color red
+    		border-radius 50%
 </style>
--- src/components/Screen7.vue
+++ src/components/Screen7.vue
@@ -1,13 +1,167 @@
 <template>
-    
+	<div class="section roadmap-section">
+		<div class="container">
+			<h1 class="section-title is-center">
+	    		Roadmap
+	    	</h1>
+		</div>
+
+		<div class="roadmap-slides">
+    		<div class="topline"></div>
+    		<div class="slides-body">
+    			<div class="slide" v-for="(slide, slideIndex) in slides" :key="slideIndex">
+    				<div class="line"></div>
+    				<div class="slide-content">
+    					<h1 class="slide-title">
+    						{{ slide.title }}
+    					</h1>
+    					<i class="date">{{ slide.date }}</i>
+    					<div class="responsible">
+    						<div class="avatar" v-for="resp in slide.responsibles"></div>
+    					</div>
+    					<div class="slide-progress">
+    						<div class="progress-line"></div>
+    						<div class="state">
+    							<p class="status">
+    								State:
+    								<span class="bold">{{ slide.state }}</span>
+    							</p>
+    							<p class="count">{{ slide.progress }}%</p>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+	</div>
 </template>
 
 <script>
 export default {
-    name: 'Screen7'
+    name: 'Roadmap',
+    data () {
+    	return {
+    		slides: [{
+    			title: 'Launch and publication MVP version of ALEHUB in centralised mode',
+    			date: 'Sunday, April 15, 2018',
+    			responsibles: [1, 2, 3],
+    			state: 'deployed',
+    			progress: 100
+    		}, {
+    			title: 'YellowPaper publication – description of  AleVirtualMachine running smart-contracts',
+    			date: 'Wednesday, August 1, 2018',
+    			responsibles: [1],
+    			state: 'Review',
+    			progress: 72
+    		}, {
+    			title: 'Launch of full ALEHUB version in centralised mode',
+    			date: 'Saturday, September 1, 2018',
+    			responsibles: [1, 2],
+    			state: 'Implementing',
+    			progress: 24
+    		}]
+    	}
+    }
 }
 </script>
 
-<style lang="stylus">
-    
+<style lang="stylus" scoped>
+
+    .section
+    	padding 71px 0
+    	padding-bottom 130px
+
+    .roadmap-section
+    	background-color #343a49
+
+    .section-title
+    	font-family MuseoSansCyrl300
+    	font-size 3em
+    	font-weight 300
+    	font-style normal
+    	font-stretch normal
+    	line-height normal
+    	letter-spacing normal
+    	text-align center
+    	color #ffffff
+    	margin 24px 0
+
+    .is-center
+    	text-align center
+
+    .is-white
+    	color #ffffff
+
+    .roadmap-slides
+    	.topline
+    		width 100%
+    		height 2px
+    		background-color #ffbc00
+
+    	.slides-body
+    		display flex
+    		overflow-x scroll
+
+    		.slide
+    			min-width 512px
+    			max-width 512px
+    			margin 0 18px
+    			.slide-content
+    				background-color: rgba(255, 255, 255, 0.05);
+    				min-height 273px
+
+    			.line
+    				width 2px
+    				height 36px
+    				background #ffbc00
+    				display flex
+    				margin 0 auto
+
+    			.slide-content
+    				padding 32px 24px
+    				.slide-title
+    					font-family MuseoSansCyrl300
+    					font-size 24px
+    					font-weight 300
+    					font-style normal
+    					font-stretch normal
+    					line-height 1.5
+    					letter-spacing normal
+    					color #ffffff
+
+    				.date
+    					font-family MuseoSansCyrl300
+						font-size 14px
+						font-weight 300
+						font-style normal
+						font-stretch normal
+						line-height normal
+						letter-spacing normal
+						text-align left
+						color #ffffff
+
+					.responsible
+						width 100%;
+						height 48px
+						margin 16px 0
+						display flex
+
+						.avatar
+							width 48px;
+							height 48px;
+							background-color red
+							border-radius 50%
+
+						.avatar:not(:first-child)
+							margin-left 12px
+
+					.slide-progress
+						.progress-line
+							margin 10px 0
+							height 4px
+							background-color #ffbc00
+							box-shadow 0 0 2px 0 rgba(255, 188, 0, 0.7), 0 0 8px 0 rgba(255, 188, 0, 0.3)
+						.state
+							display flex
+							justify-content space-between
 </style>
