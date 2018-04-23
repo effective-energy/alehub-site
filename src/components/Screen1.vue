2deacd0e01e85f983770a901c4d91c66547b4e67
Denis <denisqwe41@gmail.com>
2018-04-24 00:27:43 +0300

    First and second screens

--- src/components/Screen1.vue
+++ src/components/Screen1.vue
@@ -1,91 +1,147 @@
 <template>
-    <div class="container-fluid">
-        <section id="screen1">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="screen1 title">Welcome to the future <br> of the HR industry</h1>
+    <section id="screen1">
+        <div class="first-screen">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="screen1 title">Welcome to the future <br> of the HR industry</h1>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-7">
-                    <div class="countdown">
-                        <h2 class="title">
-                            Round 1 will begin:
-                        </h2>
-                        <div class="timer">
-                            <div class="days">
-                                <div class="numbers">
-                                    <div class="first">0</div>
-                                    <div class="second">1</div>
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="countdown">
+                            <h2 class="title">
+                                Round 1 will begin:
+                            </h2>
+                            <div class="timer">
+                                <div class="days">
+                                    <div class="numbers">
+                                        <div class="first">0</div>
+                                        <div class="second">1</div>
+                                    </div>
+                                    <div class="title">Days</div>
                                 </div>
-                                <div class="title">Days</div>
-                            </div>
-                            <div class="hours">
-                                <div class="numbers">
-                                    <div class="first">2</div>
-                                    <div class="second">4</div>
+                                <div class="hours">
+                                    <div class="numbers">
+                                        <div class="first">2</div>
+                                        <div class="second">4</div>
+                                    </div>
+                                    <div class="title">Hours</div>
                                 </div>
-                                <div class="title">Hours</div>
-                            </div>
-                            <div class="minutes">
-                                <div class="numbers">
-                                    <div class="first">5</div>
-                                    <div class="second">9</div>
+                                <div class="minutes">
+                                    <div class="numbers">
+                                        <div class="first">5</div>
+                                        <div class="second">9</div>
+                                    </div>
+                                    <div class="title">Minutes</div>
                                 </div>
-                                <div class="title">Minutes</div>
                             </div>
                         </div>
-                    </div>
-                    <button type="button" class="btn btn-buy-tokens">Buy tokens</button>
-                    <div class="bonus-desc">
-                        <span>Bonus 25% up to 7 April 2018</span>
-                        <span>Now not available</span>
-                    </div>
+                        <button type="button" class="btn btn-buy-tokens">Buy tokens</button>
+                        <div class="bonus-desc">
+                            <span>Bonus 25% up to 7 April 2018</span>
+                            <span>Now not available</span>
+                        </div>
 
-                    <div class="crypto">
-                        <div class="title">Accepted for collection:</div>
-                        <div class="collection">
-                            <div class="item">
-                                <img src="../../static/images/btc.svg" alt="">
-                                <div class="description">
-                                    <span class="count">2,389 ALE</span>
-                                    <span class="name">BTC</span>
+                        <div class="crypto">
+                            <div class="title">Accepted for collection:</div>
+                            <div class="collection">
+                                <div class="item">
+                                    <div class="cur-logo">
+                                        <img src="../../static/images/btc.svg" alt="">
+                                    </div>
+                                    <div class="description">
+                                        <span class="count">2,389 ALE</span>
+                                        <span class="name">BTC</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="item">
-                                <img src="../../static/images/eth.svg" alt="">
-                                <div class="description">
-                                    <span class="count">1,337 ALE</span>
-                                    <span class="name">ETH</span>
+                                <div class="item">
+                                    <div class="cur-logo">
+                                        <img src="../../static/images/eth.svg" alt="">
+                                    </div>
+                                    <div class="description">
+                                        <span class="count">1,337 ALE</span>
+                                        <span class="name">ETH</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="item">
-                                <img src="../../static/images/bch.svg" alt="">
-                                <div class="description">
-                                    <span class="count">2,228 ALE</span>
-                                    <span class="name">BCH</span>
+                                <div class="item">
+                                    <div class="cur-logo">
+                                        <img src="../../static/images/bch.svg" alt="">
+                                    </div>
+                                    <div class="description">
+                                        <span class="count">2,228 ALE</span>
+                                        <span class="name">BCH</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="item">
-                                <img src="../../static/images/ltc.svg" alt="">
-                                <div class="description">
-                                    <span class="count">1,488 ALE</span>
-                                    <span class="name">LTC</span>
+                                <div class="item">
+                                    <div class="cur-logo">
+                                        <img src="../../static/images/ltc.svg" alt="">
+                                    </div>
+                                    <div class="description">
+                                        <span class="count">1,488 ALE</span>
+                                        <span class="name">LTC</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="item">
-                                <img src="../../static/images/dash.svg" alt="">
-                                <div class="description">
-                                    <span class="count">2,228 ALE</span>
-                                    <span class="name">DASH</span>
+                                <div class="item">
+                                    <div class="cur-logo">
+                                        <img src="../../static/images/dash.svg" alt="">
+                                    </div>
+                                    <div class="description">
+                                        <span class="count">2,228 ALE</span>
+                                        <span class="name">DASH</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
+            <div class="container-fluid social-line">
+                <div class="line"></div>
+                <a href="#" class="social-item fb"></a>
+                <a href="#" class="social-item ins"></a>
+                <a href="#" class="social-item yt"></a>
+                <a href="#" class="social-item tw"></a>
+                <a href="#" class="social-item tg"></a>
+                <a href="#" class="social-item vk"></a>
+            </div>
+        </div>
+        <div class="container-fluid partners">
+            <div class="title">Our partners:</div>
+            <img src="../../static/images/itmo.png" alt="">
+            <img src="../../static/images/crypto.png" alt="">
+            <img src="../../static/images/beer.png" alt="">
+            <img src="../../static/images/serokell.png" alt="">
+        </div>
+        <div class="container-fluid what-is">
+            <div class="row">
+                <div class="col-lg-6 promo">
+                    <img src="../../static/images/desctop.png" class="desktop" alt="">
+                    <a href="#" class="btn btn-black"><img src="../../static/images/request-ic.svg" alt="">Download</a>
+                </div>
+                <div class="col-lg-6 desc">
+                    <h1 class="title">What is Alehub</h1>
+                    <h3 class="subtitle">
+                        WE ARE ATTRACTING BLOCKCHAIN TECHNOLOGY IN THE WORLD OF HR
+                    </h3>
+                    <p class="description">
+                        The ALE product is primarily a service for consumers to find 
+                        counterparties for projects in the IT field and to manage 
+                        these projects at the management and financial level. On the 
+                        one hand, they are programmers or their associations, and on 
+                        the other hand, they are IT Customers. ALE in this sense is 
+                        an online distributed information and financial platform / 
+                        project management system, the location and interaction of 
+                        project parties (in the first stage of IT projects).
+                    </p>
+                    <div class="buttons">
+                        <a href="#" class="btn btn-yellow">White paper</a>
+                        <a href="#" class="btn btn-yellow">Technical details of pre ICO</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
