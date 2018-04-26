db0c9e042ce7422600619570679270323a0d59de
zhiskar <zhiskar.reventlov@gmail.com>
2018-04-26 15:03:05 +0300

    Update slider

--- src/components/Screen5.vue
+++ src/components/Screen5.vue
@@ -13,7 +13,7 @@
             <div class="serokell">
                 <p>Serokell</p>
 
-                <div class="images">
+                <div class="images" id="serokell-gallery">
                     <div class="image"
                          v-for="(member, i) in team.serokell" :key="i"
                          :style="{ 'background-color': (i % 2 === 0) ? '#e8ebef' : '#abb8c6' }">
@@ -41,39 +41,45 @@
                 </div>
             </div>
 
-            <div class="effective-energy">
+            <div class="effective-energy" style="width: 100%;">
                 <p>Effective Energy team</p>
 
-                <div class="wrap">
-                    <div class="b-carousel js-carousel">
-                        <button class="b-carousel__prev js-carousel__prev"></button>
-                        <button class="b-carousel__next js-carousel__next"></button>
-                        <div class="b-carousel__wrap js-carousel__wrap">
-                            <div class="image b-carousel__item"
-                                 v-for="(member, i) in team.energy" :key="i">
+                <div style="width: 100%; display: flex; justify-content: center;">
+                    <button class="b-carousel__prev js-carousel__prev" style="background: transparent; border: none; cursor: pointer;">
+                        <img src="../../static/images/arrow-left-dark.svg" alt="prev">
+                    </button>
 
-                                <div style="margin: 0 15px; width: 100%;">
-                                    <div style="padding: 40px 20px 0 20px;"
-                                         :style="{ 'background-color': (i % 2 === 0) ? '#e8ebef' : '#abb8c6' }">
-                                        <img class="layer__bottom b-carousel__img"
-                                             :src="member.src"
-                                             :alt="member.name">
+                    <div class="wrap" style="display: flex; justify-content: center;" id="effective-energy">
+                        <div class="b-carousel js-carousel">
+                            <!--<button class="b-carousel__prev js-carousel__prev"></button>-->
+                            <!--<button class="b-carousel__next js-carousel__next"></button>-->
+                            <div class="b-carousel__wrap js-carousel__wrap">
+                                <div class="image b-carousel__item"
+                                     v-for="(member, i) in team.energy" :key="i">
 
-                                        <div class="layer__top">
-                                            <div class="layer__text">
-                                                <h3>
-                                                    {{ member.name }}
-                                                </h3>
-                                                <p>
-                                                    {{ member.position }}
-                                                </p>
+                                    <div style="margin: 0 15px; width: 100%;">
+                                        <div style="padding: 40px 20px 0 20px;"
+                                             :style="{ 'background-color': (i % 2 === 0) ? '#e8ebef' : '#abb8c6' }">
+                                            <img class="layer__bottom b-carousel__img"
+                                                 :src="member.src"
+                                                 :alt="member.name">
 
-                                                <div class="icons">
-                                                    <img class="telegram" src="../../static/images/telegram-ic.svg"
-                                                         alt="telegram">
-                                                    <img class="vk" src="../../static/images/vk.svg" alt="vk">
-                                                    <img class="fb" src="../../static/images/fb.svg" alt="fb">
-                                                    <img class="in" src="../../static/images/in.svg" alt="in">
+                                            <div class="layer__top">
+                                                <div class="layer__text">
+                                                    <h3>
+                                                        {{ member.name }}
+                                                    </h3>
+                                                    <p>
+                                                        {{ member.position }}
+                                                    </p>
+
+                                                    <div class="icons">
+                                                        <img class="telegram" src="../../static/images/telegram-ic.svg"
+                                                             alt="telegram">
+                                                        <img class="vk" src="../../static/images/vk.svg" alt="vk">
+                                                        <img class="fb" src="../../static/images/fb.svg" alt="fb">
+                                                        <img class="in" src="../../static/images/in.svg" alt="in">
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -82,6 +88,10 @@
                             </div>
                         </div>
                     </div>
+
+                    <button class="b-carousel__next js-carousel__next">
+                        <img src="../../static/images/arrow-left-dark.svg" alt="prev" style="transform: rotate(180deg);">
+                    </button>
                 </div>
             </div>
         </div>
@@ -253,7 +263,6 @@
         mounted() {
             function Timer(callback, delay) {
 
-                /* Private properties */
                 let timerId, start, remaining = delay;
 
                 /* Public methods */
@@ -281,39 +290,40 @@
                 this.resume();
             }
 
-            function Carousel(setting) {
+            function Carousel(settings) {
 
                 let privates = {},
                     xDown, yDown, xUp, yUp, xDiff, yDiff;
 
                 privates.default = {
                     touch: true,
-                    autoplay: false,
+                    autoplay: true,
                     autoplayDelay: 3000,
                     pauseOnFocus: true,
                     pauseOnHover: true
                 };
 
-                privates.setting = Object.assign(privates.default, setting);
+                privates.settings = Object.assign(privates.default, settings);
 
                 privates.isAnimationEnd = true;
 
                 privates.sel = {
-                    wrap: document.querySelector(privates.setting.wrap),
-                    children: document.querySelector(privates.setting.wrap).children,
-                    prev: document.querySelector(privates.setting.prev),
-                    next: document.querySelector(privates.setting.next)
+                    wrap: document.querySelector(privates.settings.wrap),
+                    children: document.querySelector(privates.settings.wrap).children,
+                    prev: document.querySelector(privates.settings.prev),
+                    next: document.querySelector(privates.settings.next)
                 };
 
                 privates.opt = {
                     position: 0,
-                    max_position: document.querySelector(privates.setting.wrap).children.length - 4
+                    max_position: document.querySelector(privates.settings.wrap).children.length - 3
                 };
 
                 privates.sel.wrap.appendChild(privates.sel.children[0].cloneNode(true));
 
                 // Prev slide
                 this.prev_slide = () => {
+
                     if(!privates.isAnimationEnd) {
                         return;
                     }
@@ -323,19 +333,19 @@
                     --privates.opt.position;
 
                     if(privates.opt.position < 0) {
-                        privates.sel.wrap.style['transform'] = `translateX(-${privates.opt.max_position * 20}%)`;
+                        privates.sel.wrap.style['transform'] = `translateX(-${privates.opt.max_position * 25}%)`;
                         privates.opt.position = privates.opt.max_position - 1;
                     }
 
-                    // setTimeout(() => {
-                    //     privates.sel.wrap.style['transform'] = `translateX(-${privates.opt.position * 20}%)`;
-                    // }, 10);
+                    setTimeout(() => {
+                        privates.sel.wrap.style['transform'] = `translateX(-${privates.opt.position * 25}%)`;
+                    }, 10);
 
-                    // privates.sel.wrap.addEventListener('transitionend', () => {
-                    //     privates.isAnimationEnd = true;
-                    // });
+                    privates.sel.wrap.addEventListener('transitionend', () => {
+                        privates.isAnimationEnd = true;
+                    });
 
-                    if(privates.setting.autoplay === true) {
+                    if(privates.settings.autoplay === true) {
                         privates.timer.become();
                     }
                 };
@@ -353,7 +363,7 @@
                         ++privates.opt.position;
                     }
 
-                    privates.sel.wrap.style['transform'] = `translateX(-${privates.opt.position * 20}%)`;
+                    privates.sel.wrap.style['transform'] = `translateX(-${privates.opt.position * 25}%)`;
 
                     privates.sel.wrap.addEventListener('transitionend', () => {
                         if(privates.opt.position >= privates.opt.max_position) {
@@ -364,14 +374,14 @@
                         privates.isAnimationEnd = true;
                     });
 
-                    if(privates.setting.autoplay === true) {
+                    if(privates.settings.autoplay === true) {
                         privates.timer.become();
                     }
                 };
 
                 // Autoplay
-                if(privates.setting.autoplay === true) {
-                    privates.timer = new Timer(this.next_slide, privates.setting.autoplayDelay);
+                if(privates.settings.autoplay === true) {
+                    privates.timer = new Timer(this.next_slide, privates.settings.autoplayDelay);
                 }
 
 
@@ -389,13 +399,13 @@
                 }
 
                 // Touch events
-                if(privates.setting.touch === true) {
+                if(privates.settings.touch === true) {
                     privates.sel.wrap.addEventListener('touchstart', privates.hts, false);
                     privates.sel.wrap.addEventListener('touchmove', privates.htm, false);
                 }
 
                 // Pause on hover
-                if(privates.setting.autoplay === true && privates.setting.pauseOnHover === true) {
+                if(privates.settings.autoplay === true && privates.settings.pauseOnHover === true) {
                     privates.sel.wrap.addEventListener('mouseenter', () => {
                         privates.timer.pause();
                     });
@@ -435,11 +445,36 @@
                 prev: '.js-carousel__prev',
                 next: '.js-carousel__next'
             });
+
+            document.getElementById('effective-energy').style.width = document.getElementById('serokell-gallery').offsetWidth + 'px';
         }
     }
 </script>
 
 <style lang="stylus" scoped>
+    .b-carousel__prev
+        margin-right 20px
+
+    .b-carousel__next
+        margin-left 20px
+
+    .b-carousel__next
+        &:active
+            transform translateX(20px)
+
+    .b-carousel__prev
+        &:active
+            transform translateX(-20px)
+
+    .b-carousel__prev, .b-carousel__next
+        background transparent
+        border none
+        cursor pointer
+        transition transform 0.5s ease
+
+        &:focus
+            outline 0
+
 
     .b-carousel
         width 100%
@@ -447,28 +482,6 @@
         position relative
         box-sizing border-box
 
-    .b-carousel__prev,
-    .b-carousel__next
-        position absolute
-        top 50%
-        left 20px
-        width 50px
-        height 50px
-        background #fff
-        transform translateY(-50%) translateZ(0)
-        cursor pointer
-        text-indent 100%
-        white-space nowrap
-        overflow hidden
-        z-index 3
-
-    .b-carousel__prev
-        background-color coral
-
-    .b-carousel__next
-        background-color #fda503
-        left auto
-        right 20px
 
     .b-carousel__wrap
         display flex
@@ -479,7 +492,7 @@
         height 100%
 
     .b-carousel__item
-        flex 0 0 20%
+        flex 0 0 25%
         overflow hidden
         display flex
         align-items center
