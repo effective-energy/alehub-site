36c508db75f6900bc1f76fecd7de0ac4fdb6f26f
zhiskar <zhiskar.reventlov@gmail.com>
2018-05-04 12:54:12 +0300

    Update subscribe in FooterBlock

--- src/components/TeamSection.vue
+++ src/components/TeamSection.vue
@@ -67,7 +67,7 @@
                         :num-items-in-wrap="numItemsInWrap"/>
             </div>
         </div>
-        <div class="advisors" id="advisors">
+        <div class="advisors" id="advisors" v-if="false">
             <h3 class="title">
                 {{$t('advisors.title')}}
             </h3>
