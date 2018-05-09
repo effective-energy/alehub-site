e302c67641c1332d9bac54368e19c585ee95a04f
zhiskar <zhiskar.reventlov@gmail.com>
2018-05-09 18:19:22 +0300

    Fix incorrect dates, fix adaptivity

--- src/i18n.js
+++ src/i18n.js
@@ -578,7 +578,7 @@ const messages = {
                     minutes: 'Минут'
                 },
                 btnBuyTokens: 'Купить токены',
-                bonus: 'Бонус 25% до 10 Июня 2018',
+                bonus: 'Бонус 20% до 25 Июня 2018',
                 notAvailable: 'В данный момент недоступно'
             },
             acceptedCrypto: {
@@ -1093,7 +1093,7 @@ const messages = {
                     minutes: 'Minutes'
                 },
                 btnBuyTokens: 'Buy tokens',
-                bonus: 'Bonus 25% up to 7 April 2018',
+                bonus: 'Bonus 20% up to 25 June 2018',
                 notAvailable: 'Now not available'
             },
             acceptedCrypto: {
@@ -1608,7 +1608,7 @@ const messages = {
                     minutes: 'Minutes'
                 },
                 btnBuyTokens: 'Acheter des jetons',
-                bonus: "Bonus de 25% jusqu'au 7 avril 2018",
+                bonus: "Bonus de 20% jusqu'au 25 juin 2018",
                 notAvailable: 'Maintenant non disponible'
             },
             acceptedCrypto: {
@@ -2128,7 +2128,7 @@ const messages = {
                     minutes: 'Protokoll'
                 },
                 btnBuyTokens: 'Kaufe Token',
-                bonus: 'Bonus 25% bis zum 7. April 2018',
+                bonus: 'Bonus 20% bis zum 25 Juni 2018',
                 notAvailable: 'Jetzt nicht verfügbar'
             },
             acceptedCrypto: {
@@ -3167,7 +3167,7 @@ const messages = {
                     minutes: '분'
                 },
                 btnBuyTokens: '보',
-                bonus: '보너스 25%7 월 2018',
+                bonus: '보너스 20%7 월 2018',
                 notAvailable: '지금 이용할 수 없'
             },
             acceptedCrypto: {
@@ -3687,7 +3687,7 @@ const messages = {
                     minutes: 'Minutos'
                 },
                 btnBuyTokens: 'Comprar fichas',
-                bonus: '25% de bonificación de hasta el 7 de abril de 2018',
+                bonus: '20% de bonificación de hasta el 25 de junio de 2018',
                 notAvailable: 'Ahora no está disponible'
             },
             acceptedCrypto: {
