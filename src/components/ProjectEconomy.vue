10de0e2e4dac37670aa144173b941e046b5ebdd3
im <zhitkov100@gmal.com>
2018-06-27 18:20:52 +0300

    back project economy

--- src/components/ProjectEconomy.vue
+++ src/components/ProjectEconomy.vue
@@ -12,11 +12,11 @@
                 </div>
             </div>
             <div class="row">
-                <div v-if="this.$i18n.locale === 'ru'" class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
-                    <img class="schema" src="../../static/images/project-economy-schema-rus.png"
-                         alt="project economy schema rus">
-                </div>
-                <div v-else class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                <!--<div v-if="this.$i18n.locale === 'ru'" class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">-->
+                    <!--<img class="schema" src="../../static/images/project-economy-schema-rus.png"-->
+                         <!--alt="project economy schema rus">-->
+                <!--</div>-->
+                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
                     <img class="schema" src="../../static/images/project-economy-schema.png"
                          alt="project economy schema">
                 </div>
