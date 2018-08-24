cfde253ef85cb0aea23511512555d2d97a4f6183
evg <evg@evg.evg>
2018-08-24 17:48:43 +0300

    massmedia change pics to on filter

--- src/components/layouts/mass-media/MassMediaBlock.vue
+++ src/components/layouts/mass-media/MassMediaBlock.vue
@@ -13,16 +13,18 @@
                :href="item.href"
                @mouseenter="enterMediaHover(item)"
                @mouseleave="leaveMediaHover(item)">
-                <img class="light"
-                     :class="{ 'light__transparent': item.isHover }"
-                     :src="item.src"
+                <!--:class="{ 'light__transparent': item.isHover }"
+                    :src="item.src"
+                    class="light"-->
+                <img class="filter"
+                     :src="item.hoverSrc"
                      :alt="item.title">
-                <transition name="fade-media-hover">
-                    <img class="hover"
-                         :src="item.hoverSrc"
-                         :alt="item.title"
-                         v-if="item.isHover">
-                </transition>
+                <!--<transition name="fade-media-hover">-->
+                    <!--<img class="hover"-->
+                         <!--:src="item.hoverSrc"-->
+                         <!--:alt="item.title"-->
+                         <!--v-if="item.isHover">-->
+                <!--</transition>-->
             </a>
         </div>
     </div>
@@ -235,6 +237,12 @@
                     -webkit-transition all .4s ease-in-out
                     -o-transition all .4s ease-in-out
                     transition all .4s ease-in-out
+                .filter
+                    filter  invert(.5)
+                    transition .3s
+                    &:hover
+                        filter invert(0)
+
 
                 img
                     height 60px
