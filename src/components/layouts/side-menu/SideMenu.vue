6ede545853ca64c34b814f7f494474a426a5dc51
zhiskar <zhiskar.reventlov@gmail.com>
2018-09-18 00:21:36 +0300

    Add style to SideMenuSections.vue and SideMenuPages.vue, Add switcher in mobile to SideMenu.vue

--- src/components/layouts/side-menu/SideMenu.vue
+++ src/components/layouts/side-menu/SideMenu.vue
@@ -2,33 +2,31 @@
     <div id="side-menu"
          class="side-menu"
          :class="{ 'inactive': !show, 'dark': dark, 'yellow': yellow }">
-        <h4 class="side-menu-title">
-            Sections
-        </h4>
-        <div class="side-menu-item"
-             v-for="item in $t('navbar.menuList')"
-             @click="scrollTo(item.path)">
-            <span>
-                {{ item.name }}
-            </span>
+        <side-menu-sections v-if="sectionsActive"/>
+        <side-menu-pages v-if="pagesActive"/>
+        <side-menu-languages v-if="languagesActive"/>
+        <div>
+            <button type="button"
+                    v-for="item in inactiveSwitcher"
+                    @click="switchActiveBlock(item.id)">
+                {{ item.title }}
+            </button>
         </div>
-        <h4 class="side-menu-title">
-            Pages
-        </h4>
-        <router-link tag="div"
-                     class="side-menu-item"
-                     :to="item.to"
-                     v-for="item in $t('navbar.pages')">
-            <span>
-                {{ item.name }}
-            </span>
-        </router-link>
     </div>
 </template>
 
 <script>
+    import SideMenuSections from './SideMenuSections';
+    import SideMenuPages from './SideMenuPages';
+    import SideMenuLanguages from './SideMenuLanguages';
+
     export default {
         name: 'SideMenu',
+        components: {
+            SideMenuSections,
+            SideMenuPages,
+            SideMenuLanguages
+        },
         props: {
             show: {
                 type: Boolean,
@@ -44,9 +42,52 @@
             }
         },
         data() {
-            return {}
+            return {
+                sp: true,
+                switcher: [
+                    {
+                        id: 1,
+                        title: 'sections',
+                        active: true
+                    },
+                    {
+                        id: 2,
+                        title: 'pages',
+                        active: false
+                    },
+                    {
+                        id: 3,
+                        title: 'languages',
+                        active: false
+                    }
+                ]
+            }
+        },
+        computed: {
+            /**
+             * return list of inactive switchers
+             */
+            inactiveSwitcher: function () {
+                return this.switcher.filter(s => !s.active);
+            },
+            sectionsActive: function () {
+                return this.switcher[0].active;
+            },
+            pagesActive: function () {
+                return this.switcher[1].active;
+            },
+            languagesActive: function () {
+                return this.switcher[2].active;
+            }
         },
         methods: {
+            /**
+             * switch active block
+             */
+            switchActiveBlock: function (id) {
+                this.switcher.find(s => s.active).active = false;
+                this.switcher.find(s => s.id === id).active = true;
+            },
             /**
              * scroll window to specified block
              */
@@ -119,27 +160,27 @@
                     span
                         color #ffffff
 
-        .side-menu-title
-            font-size 1.1em
-            font-weight 500
-            font-family MuseoSansCyrl500
-            padding 15px 30px
-            margin 0
-
-        .side-menu-item
-            cursor pointer
-            padding 10px 60px
-            position relative
-            transition all .2s ease
-
-            &:hover
-                background-color #ffd24f
-
-            span
-                font-family MuseoSansCyrl500
-                font-size 1.1em
-                font-weight 500
-
         @media (max-width 420px)
             padding 0
+
+    /*.side-menu-title*/
+    /*font-size 1.1em*/
+    /*font-weight 500*/
+    /*font-family MuseoSansCyrl500*/
+    /*padding 15px 30px*/
+    /*margin 0*/
+
+    /*.side-menu-item*/
+    /*cursor pointer*/
+    /*padding 10px 60px*/
+    /*position relative*/
+    /*transition all .2s ease*/
+
+    /*&:hover*/
+    /*background-color #ffd24f*/
+
+    /*span*/
+    /*font-family MuseoSansCyrl500*/
+    /*font-size 1.1em*/
+    /*font-weight 500*/
 </style>
--- src/components/layouts/side-menu/SideMenuSections.vue
+++ src/components/layouts/side-menu/SideMenuSections.vue
@@ -1,15 +1,44 @@
 <template>
-    <div class="side-menu-pages">
-
+    <div class="side-menu-sections">
+        <h4 class="side-menu-title">
+            Sections
+        </h4>
+        <div class="side-menu-item"
+             v-for="item in $t('navbar.menuList')"
+             @click="scrollTo(item.path)">
+            <span>
+                {{ item.name }}
+            </span>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "SideMenuSections"
+        name: 'SideMenuSections'
     }
 </script>
 
 <style lang="stylus" scoped>
+    .side-menu-sections
+        .side-menu-title
+            font-size 1.1em
+            font-weight 500
+            font-family MuseoSansCyrl500
+            padding 15px 30px
+            margin 0
 
+        .side-menu-item
+            cursor pointer
+            padding 10px 60px
+            position relative
+            transition all .2s ease
+
+            &:hover
+                background-color #ffd24f
+
+            span
+                font-family MuseoSansCyrl500
+                font-size 1.1em
+                font-weight 500
 </style>
