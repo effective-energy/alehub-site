54db7d3784cce49f678ddce79a78c2df81bd14f2
zhiskar <zhiskar.reventlov@gmail.com>
2018-10-15 23:03:53 +0300

    Add SectionSlogan.vue component

---
+++ src/components/SectionSlogan.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="section-slogan">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SectionSlogan',
+        data: () => ({
+
+        })
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
