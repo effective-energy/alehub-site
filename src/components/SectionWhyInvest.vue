8bd0af24712685dd485d188383c65dd509c7b6af
zhiskar <zhiskar.reventlov@gmail.com>
2018-10-15 23:05:31 +0300

    Add SectionWhyInvest.vue component

---
+++ src/components/SectionWhyInvest.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="section-why-invest">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SectionWhyInvest',
+        data: () => ({
+
+        }),
+        computed: {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
