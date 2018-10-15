9857ece1ffa1d5a642f4c59b713a5cba0ba4a541
zhiskar <zhiskar.reventlov@gmail.com>
2018-10-15 23:02:39 +0300

    Add SectionMain.vue component

---
+++ src/components/SectionMain.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="section-main">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SectionMain',
+        data: () => ({
+
+        })
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
