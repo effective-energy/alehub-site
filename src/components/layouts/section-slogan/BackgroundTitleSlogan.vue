dbe74c440edbb1dbe53398c50669fc9c8f224a26
zhiskar <zhiskar.reventlov@gmail.com>
2018-10-16 09:06:13 +0300

    Add BackgroundTitleSlogan.vue component

---
+++ src/components/layouts/section-slogan/BackgroundTitleSlogan.vue
@@ -0,0 +1,23 @@
+<template>
+    <background-title :title="title"
+                      :blue="blue"/>
+</template>
+
+<script>
+    import BackgroundTitle from '../background-title/BackgroundTitle';
+
+    export default {
+        name: 'BackgroundTitleSlogan',
+        components: {
+            BackgroundTitle
+        },
+        data: () => ({
+            title: 'World\'s first',
+            blue: false
+        })
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/components/section/SectionSlogan.vue
+++ src/components/section/SectionSlogan.vue
@@ -1,18 +1,27 @@
 <template>
     <div class="section-slogan">
         section slogan
+        <background-title-slogan/>
     </div>
 </template>
 
 <script>
+    import BackgroundTitleSlogan from '../layouts/section-slogan/BackgroundTitleSlogan';
+
     export default {
         name: 'SectionSlogan',
-        data: () => ({
-
-        })
+        components: {
+            BackgroundTitleSlogan
+        },
+        data: () => ({})
     }
 </script>
 
 <style lang="stylus" scoped>
-
+    .section-slogan
+        z-index 2
+        position relative
+        height 100vh
+        background-color #020449
+        color white
 </style>
