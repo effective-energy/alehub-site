8f378946e90838b20c8a476a64e644c497b0cef8
zhiskar <zhiskar.reventlov@gmail.com>
2018-10-19 01:19:31 +0300

    Add SubtitleWelcome.vue component, Update SectionWelcome.vue

---
+++ src/components/layouts/section-welcome/SubtitleWelcome.vue
@@ -0,0 +1,21 @@
+<template>
+    <subtitle-section :subtitle="subtitle"/>
+</template>
+
+<script>
+    import SubtitleSection from '../text/SubtitleSection';
+
+    export default {
+        name: 'SubtitleWelcome',
+        components: {
+            SubtitleSection
+        },
+        data: () => ({
+            subtitle: 'Сделай свой вклад'
+        })
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/components/sections/SectionWelcome.vue
+++ src/components/sections/SectionWelcome.vue
@@ -1,23 +1,48 @@
 <template>
     <div class="section-welcome">
-        <title-welcome/>
+        <v-container grid-list-md
+                     class="section-slogan-container">
+            <v-layout align-center row>
+                <v-flex>
+                    <v-layout column>
+                        <v-flex>
+                            <title-welcome/>
+                        </v-flex>
+                        <v-flex>
+                            <subtitle-welcome/>
+                        </v-flex>
+                        <v-flex>
+
+                        </v-flex>
+                    </v-layout>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
 <script>
     import TitleWelcome from '../layouts/section-welcome/TitleWelcome';
+    import SubtitleWelcome from '../layouts/section-welcome/SubtitleWelcome';
 
     export default {
         name: 'SectionWelcome',
         components: {
-            TitleWelcome
+            TitleWelcome,
+            SubtitleWelcome
         }
     }
 </script>
 
 <style lang="stylus" scoped>
-.section-welcome
-    height 50vh
-    background-color #020449
-    color white
+    .section-welcome
+        height 50vh
+        background-color #020449
+        color white
+
+        .container
+            height 100%
+
+            .layout
+                height 100%
 </style>
