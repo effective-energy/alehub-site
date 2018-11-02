03f9d3f3d83b5b5d6d6fd6f1f13f93c46d475deb
zhiskar <zhiskar.reventlov@gmail.com>
2018-11-02 17:19:06 +0300

    Refactor style in SectionTeamAdvisor

--- src/components/sections/SectionTeamAdvisors.vue
+++ src/components/sections/SectionTeamAdvisors.vue
@@ -95,54 +95,6 @@
             -webkit-filter grayscale(0)
             filter grayscale(0)
 
-    .member
-        display flex
-        flex-direction column
-        align-items center
-
-    .name
-        font-family: MuseoSansCyrl500;
-        font-size: 24px;
-        font-weight: 500;
-        font-style: normal;
-        font-stretch: normal;
-        line-height: 1.33;
-        letter-spacing: normal;
-        text-align: center;
-        color: #ffffff;
-        margin-top 24px
-
-    .position
-        opacity: 0.72;
-        font-family: MuseoSansCyrl;
-        font-size: 16px;
-        font-weight: 300;
-        font-style: normal;
-        font-stretch: normal;
-        line-height: 1.5;
-        letter-spacing: normal;
-        text-align: center;
-        color: #ffffff;
-        margin-top 16px
-
-    .text
-        font-family: MuseoSansCyrl300;
-        font-size: 16px;
-        font-weight: 300;
-        font-style: normal;
-        font-stretch: normal;
-        line-height: 1.5;
-        letter-spacing: normal;
-        text-align: center;
-        color: #ffffff;
-        margin-top 16px
-
-    .photo
-        height 160px
-        width 160px
-        border-radius 50%
-        background-color #ff8c8a
-
     .section-team-advisors
         height 90vh
 </style>
